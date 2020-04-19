3821554ac0fa518f8ae101dd8ec461ce0d251008
pinchazer <pinchazer@mail.ru>
2020-04-19 13:41:24 +0300

    First

---
+++ currencybot.py
@@ -0,0 +1,43 @@
+import logging
+#import dataobtain
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update, context):
+    update.message.reply_text("Здарова, просто нажми на эту хрень /kurs")
+
+
+
+def kurs(update, context):
+    #caption = 'Текущий курс {}'.format(dataobtain.priceNow)
+    #dataobtain.draw_pic(dataobtain.dfUSDRUB)
+    with open(r'.\pic.png', 'rb') as f:
+        update.message.reply_photo(photo=f, caption='caption')
+
+def callback_minute(context):
+    context.bot.send_message(chat_id=context.job.context, text='дарова')
+
+def teller(update, context):
+    context.job_queue.run_repeating(callback_minute, 3, context=update.message.chat_id)
+
+def main():
+    updater = Updater("992561955:AAEbZ8e5UzS1qGa4LBEOKDg6tJfigzl9GDo", use_context=True)
+    dp = updater.dispatcher
+    j = updater.job_queue
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('kurs', kurs))
+    dp.add_handler(CommandHandler('tell', teller))
+    #j.run_repeating(callback_minute, interval=3, first=0, context=)
+
+    #dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
---
+++ dataobtain.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+USDRUB = yf.Ticker("RUB=X")
+priceNow = round(USDRUB.info['regularMarketPrice'], 2)
+#datenow = pd.to_datetime('today').strftime('%Y-%m-%d')
+#datebefore = (pd.to_datetime('today') - pd.Timedelta('60D')).strftime('%Y-%m-%d')
+#dfUSDRUB = yf.download("RUB=X", start=datebefore, end=datenow, interval='1d')
+dfUSDRUB = USDRUB.history(period='2mo', interval='1d')
+
+def draw_pic(dataframe):
+    sns.set()
+    plt.figure(figsize=(10,7))
+    plt.title('USD/RUB', fontsize=16, pad=30)
+    plt.ylabel('RUB per USD')
+    plt.xticks(rotation=45)
+    plt.plot(dataframe['Open'], linewidth=2, color='Purple')
+    plt.savefig('pic.png')
+
