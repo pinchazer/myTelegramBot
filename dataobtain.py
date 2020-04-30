46cd4270e5fe0c1ac8015b22a84d0823393a522b
pinchazer <pinchazer@mail.ru>
2020-04-30 14:46:26 +0300

    ver 1.0

--- dataobtain.py
+++ dataobtain.py
@@ -1,20 +1,198 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-import seaborn as sns
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator, AutoLocator
+import pandas as pd
 
-USDRUB = yf.Ticker("RUB=X")
-priceNow = round(USDRUB.info['bid'], 2)
-#datenow = pd.to_datetime('today').strftime('%Y-%m-%d')
-#datebefore = (pd.to_datetime('today') - pd.Timedelta('60D')).strftime('%Y-%m-%d')
-#dfUSDRUB = yf.download("RUB=X", start=datebefore, end=datenow, interval='1d')
-dfUSDRUB = USDRUB.history(period='2mo', interval='1d')
 
-def draw_pic(dataframe):
-    sns.set()
-    plt.figure(figsize=(10,7))
-    plt.title('USD/RUB', fontsize=16, pad=30)
-    plt.ylabel('RUB per USD')
-    plt.xticks(rotation=45)
-    plt.plot(dataframe['Open'], linewidth=2, color='Purple')
-    plt.savefig('pic.png')
+from datetime import datetime, timezone, time
+from time import sleep
+from telegram.ext import Dispatcher
 
+#USDRUB = yf.Ticker("RUB=X")
+#priceNow = round(USDRUB.info['bid'], 2)
+# datenow = pd.to_datetime('today').strftime('%Y-%m-%d')
+# datebefore = (pd.to_datetime('today') - pd.Timedelta('60D')).strftime('%Y-%m-%d')
+# dfUSDRUB = yf.download("RUB=X", start=datebefore, end=datenow, interval='1d')
+#dfUSDRUB = USDRUB.history(period='2mo', interval='1d')
+
+
+
+
+
+transient_val_dollar = 0.0
+transient_val_euro = 0.1
+
+def daily_routine(context):
+    #while True:
+        # 86400 sec in day
+    #sleep(86400)
+    USD = yf.Ticker("RUB=X")
+    EUR = yf.Ticker("EURRUB=X")
+    dfUSD = USD.history(period='2mo', interval='1d')
+    dfEUR = EUR.history(period='2mo', interval='1d')
+    draw_pic(dfUSD, 'dol')
+    draw_pic(dfEUR, 'eur')
+
+def show_dollar(context):
+    global transient_val_dollar
+    USD = yf.Ticker("RUB=X")
+    priceNow = USD.info['bid']
+    transient_val_dollar = priceNow
+    #print('show_dollar')
+    return priceNow
+
+def show_euro(context):
+    global transient_val_euro
+    EUR = yf.Ticker("EURRUB=X")
+    priceNow = EUR.info['bid']
+    transient_val_euro = priceNow
+    return priceNow
+
+def signal_message_dollar(context):
+    context.bot.send_message(chat_id=context.job.context, text='')
+
+def signal_message_euro(context):
+    context.bot.send_message(chat_id=context.job.context, text='')
+"""
+def signal_routine(dispatcher):
+    if not isinstance(dispatcher, Dispatcher):
+        raise TypeError('{} should be Dispatcher object'.format(dispatcher))
+    while True:
+        sleep(60)
+        priceEurNow = show_euro()
+        priceDolNow = show_dollar()
+        for x in ['dol', 'eur']:
+            user_data = dispatcher.user_data[x]
+            if user_data is None:
+                continue
+            else:
+                if x == 'dol':
+                    if show_dollar() > user_data:
+                        dispatcher.job_queue.run(signal_message_dollar)
+                if x == 'eur':
+                    dispatcher.job_queue.run(signal_message_euro)
+"""
+"""
+def signal_routine(context):
+    #10:00:00 — 22:00:00
+    dp = context.job.context
+    curList = ['dol', 'eur']
+    _trig_less = False
+    _trig_more = False
+    for x in curList:
+        signalVal = dp.user_data[x]
+        if signalVal is None:
+            continue
+        elif x == 'dol':
+            if show_dollar() > signalVal:
+                if _trig_less:
+                    context.bot.send_message(chat_id=dp.user_data['chat_id'],
+                                             text='Обратите внимание на курс доллара!\n'
+                                                    'Ваш сигнал {}'.format(signalVal))
+                    pass
+                _trig_less = False
+                _trig_more = True
+            if show_dollar() < signalVal:
+                if _trig_more:
+                    context.bot.send_message(chat_id=dp.user_data['chat_id'],
+                                             text='Обратите внимание на курс доллара!\n'
+                                                    'Ваш сигнал {}'.format(signalVal))
+                _trig_less = True
+                _trig_more = False
+        elif x == 'eur':
+            if show_dollar() > signalVal:
+                if _trig_less:
+                    context.bot.send_message(chat_id=dp.user_data['chat_id'],
+                                             text='Обратите внимание на курс евро!\n'
+                                                  'Ваш сигнал {}'.format(signalVal))
+                    pass
+                _trig_less = False
+                _trig_more = True
+            if show_dollar() < signalVal:
+                if _trig_more:
+                    context.bot.send_message(chat_id=dp.user_data['chat_id'],
+                                             text='Обратите внимание на курс евро!\n'
+                                                  'Ваш сигнал {}'.format(signalVal))
+                _trig_less = True
+                _trig_more = False
+"""
+
+
+def signal_routine_dollar(context):
+    #10:00:00 — 22:00:00
+    jc = context.job.context
+    _trig_less = jc['queue_dol'].get()
+    _trig_more = jc['queue_dol'].get()
+    showDollar = transient_val_dollar
+    #print(showDollar)
+    if showDollar > float(jc['dol']):
+        if _trig_less:
+            context.bot.send_message(chat_id=jc['chat_id'],
+                                     text='Обратите внимание на курс доллара!\n'
+                                            'Ваш сигнал {}'.format(jc['dol']))
+        _trig_less = False
+        _trig_more = True
+    if showDollar < float(jc['dol']):
+        if _trig_more:
+            context.bot.send_message(chat_id=jc['chat_id'],
+                                     text='Обратите внимание на курс доллара!\n'
+                                            'Ваш сигнал {}'.format(jc['dol']))
+        _trig_less = True
+        _trig_more = False
+    jc['queue_dol'].put(_trig_less)
+    jc['queue_dol'].put(_trig_more)
+
+def signal_routine_euro(context):
+    #10:00:00 — 22:00:00
+    jc = context.job.context
+    _trig_less = jc['queue_eur'].get()
+    _trig_more = jc['queue_eur'].get()
+    showEuro = transient_val_euro
+    #print(showEuro)
+    if showEuro > float(jc['eur']):
+        if _trig_less:
+            context.bot.send_message(chat_id=jc['chat_id'],
+                                     text='Обратите внимание на курс евро!\n'
+                                            'Ваш сигнал {}'.format(jc['eur']))
+        _trig_less = False
+        _trig_more = True
+    if showEuro < float(jc['eur']):
+        if _trig_more:
+            context.bot.send_message(chat_id=jc['chat_id'],
+                                     text='Обратите внимание на курс евро!\n'
+                                            'Ваш сигнал {}'.format(jc['eur']))
+        _trig_less = True
+        _trig_more = False
+    jc['queue_eur'].put(_trig_less)
+    jc['queue_eur'].put(_trig_more)
+
+def draw_pic(dataframe, currency):
+    # check types
+    if not isinstance(dataframe, pd.DataFrame):
+        raise TypeError('{} should be Pandas.DataFrame object'.format(dataframe))
+    if not (currency == 'dol') | (currency == 'eur'):
+        raise NameError('{} should be \'dol\' or \'eur\''.format(currency))
+
+    fig, ax = plt.subplots(figsize=(10, 7))
+
+    mean_val = dataframe.loc[:, ['High', 'Low']].mean(axis=1)
+    x_values = dataframe.index.strftime('%b %d')
+
+    ax.fill_between(x_values, dataframe['High'], dataframe['Low'], color='Purple', alpha=0.2)
+    ax.plot(x_values, mean_val, linewidth=2, color='Purple')
+
+    ax.tick_params(axis='x', direction='out', length=8)
+    ax.xaxis.set_major_locator(MultipleLocator(4))
+    ax.yaxis.set_major_locator(AutoLocator())
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.grid(which='both')
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+    if currency == 'dol':
+        ax.set_title('USD/RUB', fontsize=16, pad=25)
+        ax.set_ylabel('RUB per USD')
+        fig.savefig('picDol.png')
+    if currency == 'eur':
+        ax.set_title('EUR/RUB', fontsize=16, pad=25)
+        ax.set_ylabel('RUB per EUR')
+        fig.savefig('picEur.png')
--- webhook.py
+++ webhook.py
@@ -3,12 +3,33 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_sslify import SSLify
-from queue import Queue  # in python 2 it should be "from Queue"
+from queue import Queue, Empty  # in python 2 it should be "from Queue"
 from threading import Thread
 import logging
 import telegram
 from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
+import dataobtain
+from datetime import time
+from time import sleep
+from functools import wraps
+from telegram import ChatAction
+
+#TODO написать ответ если формат ввода сообщения неправильный, при установки сигнала
+
+def send_action(action):
+    """Sends `action` while processing func command."""
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(update, context, *args, **kwargs)
+        return command_func
+    return decorator
+
+
+send_typing_action = send_action(ChatAction.TYPING)
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -18,11 +39,12 @@ app = Flask(__name__)
 sslify = SSLify(app)
 
 TOKEN = "992561955:AAEbZ8e5UzS1qGa4LBEOKDg6tJfigzl9GDo"
-my_webhook_url = 'https://199cf81e.ngrok.io'
+my_webhook_url = 'https://024d2af3.eu.ngrok.io'
 port = '88'
 
 # Stages
 DOLLAR_SIG, EURO_SIG, DEL_DOLL, DEL_EUR = range(4)
+
 
 def signals_markup_reply():
     keyboard = [
@@ -33,8 +55,8 @@ def signals_markup_reply():
     ]
     return InlineKeyboardMarkup(keyboard)
 
-def start(update, context):
 
+def start(update, context):
     custom_keyboard = [['Курс $', 'Курс \u20AC'],
                        ['Установить/удалить сигналы', 'Выход']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
@@ -43,6 +65,8 @@ def start(update, context):
         "Я могу предоставить информацию о курсах валют, а так же установить сигналы",
         reply_markup=reply_markup
     )
+    # context.user_data['chat_id'] = update.message.chat_id
+
 
 def close(update, context):
     reply_markup = telegram.ReplyKeyboardRemove()
@@ -50,60 +74,141 @@ def close(update, context):
         "До встречи! Для работы команда /start",
         reply_markup=reply_markup
     )
+    for name in ('euro_routine', 'show_euro', 'dollar_routine', 'show_dollar'):
+        for jobObj in context.job_queue.get_jobs_by_name(name):
+            jobObj.schedule_removal()
 
+
+@send_typing_action
 def dollar(update, context):
-    update.message.reply_text("dollar")
-    #TODO show dollar
-    pass
+    # update.message.reply_text("dollar")
+    caption = 'Текущий курс доллара <b>{}</b>\nДанные получены с сайта <a href="http://yahoo.com">Yahoo! Finance</a>' \
+        .format(dataobtain.show_dollar())
+    with open(r'picDol.png', 'rb') as f:
+        update.message.reply_photo(photo=f, caption=caption, parse_mode=telegram.ParseMode.HTML)
 
+
+@send_typing_action
 def euro(update, context):
-    update.message.reply_text("euro")
-    #TODO show euro
-    pass
+    # update.message.reply_text("euro")
+    caption = 'Текущий курс евро <b>{}</b>\nДанные получены с сайта <a href="http://yahoo.com">Yahoo! Finance</a>' \
+        .format(dataobtain.show_euro())
+    with open(r'picEur.png', 'rb') as f:
+        update.message.reply_photo(photo=f, caption=caption, parse_mode=telegram.ParseMode.HTML)
+
 
 def signals_menu(update, context):
     update.message.reply_text("Выберите действие:", reply_markup=signals_markup_reply())
+    context.user_data['chat_id'] = update.message.chat_id
+
 
 def del_dol(update, context):
     query = update.callback_query
     query.answer()
     query.edit_message_text(text='Выберите действие:\nсигнал для $ удален', reply_markup=signals_markup_reply())
+    for name in ('dollar_routine', 'show_dollar'):
+        for jobObj in context.job_queue.get_jobs_by_name(name):
+            jobObj.schedule_removal()
+    # query.message.reply_text('сигнал для $ удален')
     # TODO reset signal
     pass
+
 
 def del_eur(update, context):
     query = update.callback_query
     query.answer()
     query.edit_message_text(text='Выберите действие:\nсигнал для \u20AC удален', reply_markup=signals_markup_reply())
     # query.message.reply_text('сигнал для \u20AC удален')
+    for name in ('euro_routine', 'show_euro'):
+        for jobObj in context.job_queue.get_jobs_by_name(name):
+            jobObj.schedule_removal()
     # TODO reset signal
     pass
+
 
 def signal_dol(update, context):
     query = update.callback_query
     query.answer()
     user = query.from_user
     logger.info("User %s started the conversation.", user.first_name)
-    query.message.reply_text("Напишите курс $, для которого устанавливаете сигнал")
+    query.message.reply_text("Напишите курс $, для которого устанавливаете сигнал\n"
+                             "Например 65.44")
     return DOLLAR_SIG
+
 
 def signal_eur(update, context):
     query = update.callback_query
     query.answer()
     user = query.from_user
-    logger.info("User %s started the conversation.", user.first_name)
-    query.message.reply_text("Напишите курс \u20AC, для которого устанавливаете сигнал")
+    logger.info("User {} started the conversation.".format(user.first_name))
+    query.message.reply_text("Напишите курс \u20AC, для которого устанавливаете сигнал\n"
+                             "Например 75.78")
     return EURO_SIG
 
+
 def signal_dol_setup(update, context):  # part of ConversationHandler
-    update.message.reply_text("signal_dol_setup")
+    context.user_data['dol'] = update.message.text
+    for name in ('dollar_routine'):
+        #print('{}: {}'.format(name, context.job_queue.get_jobs_by_name(name)))
+        for jobObj in context.job_queue.get_jobs_by_name(name):
+            jobObj.schedule_removal()
+
+    context.user_data['queue_dol'] = Queue()
+    context.user_data['queue_dol'].put(False)
+    context.user_data['queue_dol'].put(False)
+    """
+    while True:
+        try:
+            context.user_data['queue'].get()
+        except Empty:
+            break
+    """
+    context.job_queue.run_repeating(callback=dataobtain.signal_routine_dollar, interval=20,
+                                    first=10, name='dollar_routine', context=context.user_data)
+
+    if context.job_queue.get_jobs_by_name('show_dollar') == ():
+        context.job_queue.run_repeating(callback=dataobtain.show_dollar, interval=20,
+                                        first=0, name='show_dollar')
+
+
+    update.message.reply_text("Сигнал для доллара установлен")
+    return ConversationHandler.END
+
 
 def signal_eur_setup(update, context):  # part of ConversationHandler
-    update.message.reply_text("signal_eur_setup")
+    context.user_data['eur'] = update.message.text
+    for name in ('euro_routine'):
+        for jobObj in context.job_queue.get_jobs_by_name(name):
+            jobObj.schedule_removal()
+    # print(context.job_queue)
+    # print(context.job_queue.jobs())
+    # print(context.job_queue.get_jobs_by_name('euro_routine'))
+
+    context.user_data['queue_eur'] = Queue()
+    context.user_data['queue_eur'].put(False)
+    context.user_data['queue_eur'].put(False)
+    """
+    while True:
+        try:
+            context.user_data['queue'].get()
+        except Empty:
+            break
+    """
+    context.job_queue.run_repeating(callback=dataobtain.signal_routine_euro, interval=20,
+                                    first=10, name='euro_routine', context=context.user_data)
+
+    if context.job_queue.get_jobs_by_name('show_euro') == ():
+        context.job_queue.run_repeating(callback=dataobtain.show_euro, interval=20,
+                                        first=0, name='show_euro')
+    # context.job_queue.start()
+    update.message.reply_text("Сигнал для евро установлен")
+    return ConversationHandler.END
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def setup(token):
     # Create bot, update queue and dispatcher instances
@@ -111,8 +216,10 @@ def setup(token):
     if bot.getWebhookInfo()['url'] != my_webhook_url:
         bot.set_webhook(webhook_url='{0}/:{1}/{2}'.format(my_webhook_url, port, TOKEN))
     update_queue = Queue()
+    job_queue = telegram.ext.JobQueue()
 
-    dispatcher = Dispatcher(bot, update_queue, use_context=True)
+    dispatcher = Dispatcher(bot, update_queue, use_context=True, job_queue=job_queue)
+    job_queue.set_dispatcher(dispatcher)
 
     ##### Register handlers here #####
 
@@ -133,8 +240,8 @@ def setup(token):
     dispatcher.add_handler(conv_handler)
     dispatcher.add_handler(MessageHandler(Filters.regex('^Курс \u20AC$'), euro))
     dispatcher.add_handler(MessageHandler(Filters.regex('^Курс \$$'), dollar))
-    dispatcher.add_handler(CallbackQueryHandler(del_dol, pattern='^'+str(DEL_DOLL)+'$'))
-    dispatcher.add_handler(CallbackQueryHandler(del_eur, pattern='^'+str(DEL_EUR)+'$'))
+    dispatcher.add_handler(CallbackQueryHandler(del_dol, pattern='^' + str(DEL_DOLL) + '$'))
+    dispatcher.add_handler(CallbackQueryHandler(del_eur, pattern='^' + str(DEL_EUR) + '$'))
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(MessageHandler(Filters.regex('^Установить/удалить сигналы$'), signals_menu))
     dispatcher.add_handler(MessageHandler(Filters.regex('^Выход$'), close))
@@ -150,8 +257,23 @@ def setup(token):
     return (update_queue, dispatcher)
 
 
+# use utc time
+t = time(hour=6, minute=0, second=0)
 update_queue, dp = setup(TOKEN)
+# drawpics = Thread(target=dataobtain.daily_routine)
+# drawpics.start()
+# jq = dp.job_queue
 
+dp.job_queue.run_daily(callback=dataobtain.daily_routine, days=(0, 1, 2, 3, 4),
+                       time=t, name='run_daily')
+dp.job_queue.start()
+# dp.job_queue.run_repeating(callback=dataobtain.daily_routine, interval=5, name='run_daily')
+# sleep(19)
+#print('run')
+
+
+# dp.job_queue.run_repeating(callback=dataobtain.signal_routine, interval=5, first=0, name='run_repeating')
+# dp.job_queue.start()
 
 @app.route('/')
 def hello_world():
